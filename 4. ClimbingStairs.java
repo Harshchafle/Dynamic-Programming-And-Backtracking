b4553228a9de8ac2b3d90f3f0c3a5d431a489c9c
Harsh Chafle <157059028+Harshchafle@users.noreply.github.com>
2025-03-30 14:55:20 +0530

    Update 4. ClimbingStairs.java

--- 4. ClimbingStairs.java
+++ 4. ClimbingStairs.java
@@ -1,6 +1,10 @@
 public class ClimbingStairs {
     public static void main(String[] args) {
-        System.out.println(climb(5));
+        int n = 5;
+        // System.out.println(climb(n));
+        int dp[] = new int[n];
+        Arrays.fill(dp, -1);
+        System.out.println(memo(n, dp));
     }
 
     public static int climb(int n){
@@ -11,5 +15,16 @@ public class ClimbingStairs {
         int one = climb(n-1);
         int two = climb(n-2);
         return one + two;
+    }
+
+    public static int memo(int n, int[] dp){
+        if (n < 0)
+            return 0;
+        else if (n == 0)
+            return 1;
+        if(dp[n] != -1) return dp[n];
+        int one = climb(n-1, dp);
+        int two = climb(n-2, dp);
+        return dp[n] = one + two;
     }
 }
